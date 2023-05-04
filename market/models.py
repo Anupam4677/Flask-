86def561ee4fcc0ddc5d1547e0b087e61ecdc467
Anupamprakashsingh <anupamprakash01@gmail.com>
2023-05-04 23:59:15 +0530

    Model_Relalionship

--- market/models.py
+++ market/models.py
@@ -1,17 +1,31 @@
 
 
-# db = SQLAlchemy(app)
-
-# with app.app_context():
-#     db.create_all()
-
+from flask_sqlalchemy import SQLAlchemy
 from market import db
+# from market import app
+from flask import Flask, current_app
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+class User(db.Model):
+    id = db.Column(db.Integer(),primary_key = True)
+    username = db.Column(db.String(length=30),nullable=False,unique=True)
+    email_address = db.Column(db.String(length=50),nullable=False,unique=True)
+    password_hash = db.Column(db.String(length=50),nullable=False)
+    budget = db.Column(db.Integer(),nullable=False,default=1000)
+    items = db.relationship('Item',backref='owned_user',lazy=True)
+
 class Item(db.Model):
     id = db.Column(db.Integer(),primary_key = True)
     name = db.Column(db.String(length=30),nullable=False,unique= True)
     price = db.Column(db.Integer(),nullable=False)
     barcode = db.Column(db.String(length=12),nullable=False,unique= True)
     description = db.Column(db.String(length=1024),nullable=False,unique= True)
-
+    owner = db.Column(db.Integer(),db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
