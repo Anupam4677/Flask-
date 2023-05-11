a79abb6e4192f6f7f8dc20e52daaab2f4a78db69
Anupamprakashsingh <anupamprakash01@gmail.com>
2023-05-11 13:31:50 +0530

    Added Micro

--- run.py
+++ run.py
@@ -97,6 +97,19 @@ def filter_data():
     return render_template('filter_data.html',movies= movie_dict,name=None,film='a chrismas carol')
 
 
+# JINJA2 - MACROS
+@app.route('/macros')
+def jinja_macros():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('using_macros.html', movies=movies_dict)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
